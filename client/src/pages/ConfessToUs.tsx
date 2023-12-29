ba81b3e0f90bbba502443aa95efd20db37834ca5
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-29 22:59:19 +0000

    feat: Confess form now posts submission to server

--- client/src/pages/ConfessToUs.tsx
+++ client/src/pages/ConfessToUs.tsx
@@ -1,7 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useCallback, useContext, useMemo, useState } from "react";
 import { Dropdown, Option } from "../components/Dropdown/Dropdown";
+import { MisdemeanourKind } from "../types/misdemeanours.types";
 import "./ConfessToUs.css";
-import { REASONS, Reason } from "../types/request.types";
+import axios from "axios";
+import { MisdemeanoursContext } from "../App";
+import {
+  ConfessResponse,
+  PostData,
+  REASONS,
+  Reason,
+} from "../types/request.types";
 
 const OPTIONS: Option[] = REASONS.map((x) => {
   if (x === "just-talk") return { value: x, label: " just want to talk" };
@@ -9,9 +17,53 @@ const OPTIONS: Option[] = REASONS.map((x) => {
 });
 
 export function ConfessToUs() {
+  const { misdemeanours, setMisdemeanours } = useContext(MisdemeanoursContext);
   const [reason, setReason] = useState<Reason>(REASONS[0]);
   const [subject, setSubject] = useState("");
   const [details, setDetails] = useState("");
+  const [response, setResponse] = useState<ConfessResponse>();
+
+  const onSubmit = useCallback(() => {
+    async function submit() {
+      const data: PostData = {
+        subject,
+        details,
+        reason,
+      };
+      try {
+        const response = await axios.post<ConfessResponse>(
+          "http://localhost:8080/api/confess",
+          data
+        );
+
+        setResponse(response.data);
+
+        if (response.data.success && !response.data.justTalked) {
+          const date = new Date();
+          setMisdemeanours([
+            ...misdemeanours,
+            {
+              citizenId: date.getTime(),
+              date: date.toLocaleDateString(),
+              misdemeanour: reason as MisdemeanourKind,
+              details: details,
+            },
+          ]);
+        }
+      } catch (error) {
+        setResponse({
+          success: false,
+          justTalked: false,
+          message: "ERROR: Server is not running!",
+        });
+      }
+    }
+    setSubject("");
+    setReason(REASONS[0]);
+    setDetails("");
+    setResponse(undefined);
+    submit();
+  }, [reason, subject, details]);
 
   return (
     <div className="confess-to-us">
@@ -47,7 +99,11 @@ export function ConfessToUs() {
           value={details}
           onChange={(v) => setDetails(v.currentTarget.value)}
         />
-        <button>Confess</button>
+        <button onClick={onSubmit}>Confess</button>
+        {response?.success === false && (
+          <div>Confess Failed: {response?.message}</div>
+        )}
+        {response?.success === true && <div>Confess Success</div>}
       </div>
     </div>
   );
